311eff6c4754d1dbaa6cf1a7454af21fe5402f69
ilyas <ilyasdaoudigithub07@gmail.com>
2025-10-15 16:23:25 +0200

    commit

---
+++ Desktop/challenges/ch2boucles.js
@@ -0,0 +1,14 @@
+const prompt = requit ("prompt-sync")()
+const prem = +prompt("saisir un nombre :");
+let div = 2;
+div = Number(div);
+while(prem >= div || prem === 1 || prem === 0 ){
+    if(prem === 1 || prem === 0 )
+}
+    
+
+
+
+
+    
+
